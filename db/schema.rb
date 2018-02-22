0f469709c289902a7c5d57c0235caa2af4f9430c
Wilfred Tannr Allard <rockinghamtannr@gmail.com>
2018-02-22 10:19:12 -0500

    Adding lists migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171002062101) do
+ActiveRecord::Schema.define(version: 20180222151615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "actions", force: :cascade do |t|
-    t.string "description", default: ""
-    t.string "actionable_type"
-    t.bigint "actionable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["actionable_type", "actionable_id"], name: "index_actions_on_actionable_type_and_actionable_id"
-  end
-
   create_table "boards", force: :cascade do |t|
-    t.string "title", default: "", null: false
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "cards", force: :cascade do |t|
-    t.string "title", default: "", null: false
-    t.text "description", default: "", null: false
-    t.string "labels", default: [], array: true
-    t.integer "list_id"
-    t.float "position", default: 65535.0, null: false
-    t.boolean "archived", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "due_date"
-    t.boolean "completed", default: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.text "text", default: ""
-    t.integer "card_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["card_id"], name: "index_comments_on_card_id"
   end
 
   create_table "lists", force: :cascade do |t|
-    t.string "title", default: "", null: false
     t.integer "board_id"
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "position", default: 65535.0, null: false
   end
 
 end
